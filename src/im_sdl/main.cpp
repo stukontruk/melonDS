45596e2906154eddfa46a135267d8eb24ed399df
StapleButter <thetotalworm@gmail.com>
2017-08-30 15:25:25 +0200

    get actual work started... I guess

---
+++ src/im_sdl/main.cpp
@@ -0,0 +1,82 @@
+/*
+    Copyright 2016-2017 StapleButter
+
+    This file is part of melonDS.
+
+    melonDS is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    melonDS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with melonDS. If not, see http://www.gnu.org/licenses/.
+*/
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+#include <SDL2/SDL.h>
+
+#include "../types.h"
+
+
+
+int main(int argc, char** argv)
+{
+    srand(time(NULL));
+
+    // http://stackoverflow.com/questions/14543333/joystick-wont-work-using-sdl
+    SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1");
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        printf("SDL shat itself :(\n");
+        return 1;
+    }
+
+    // shit here
+    printf("burp\n");
+
+    SDL_Quit();
+    return 0;
+}
+
+#ifdef __WIN32__
+
+#include <windows.h>
+
+int CALLBACK WinMain(HINSTANCE hinst, HINSTANCE hprev, LPSTR cmdline, int cmdshow)
+{
+    char cmdargs[16][256];
+    int arg = 0;
+    int j = 0;
+    bool inquote = false;
+    int len = strlen(cmdline);
+    for (int i = 0; i < len; i++)
+    {
+        char c = cmdline[i];
+        if (c == '\0') break;
+        if (c == '"') inquote = !inquote;
+        if (!inquote && c==' ')
+        {
+            if (arg < 16) cmdargs[arg][j] = '\0';
+            arg++;
+            j = 0;
+        }
+        else
+        {
+            if (arg < 16 && j < 255) cmdargs[arg][j] = c;
+            j++;
+        }
+    }
+    if (arg < 16) cmdargs[arg][j] = '\0';
+
+    return main(arg, (char**)cmdargs);
+}
+
+#endif
